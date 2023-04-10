f0c78de5d0071a111ff6e11161c232663ba1e71f
Alvin Lee <alvinlee4197@gmail.com>
2023-04-09 20:18:39 -0400

    Remote S3 and DynamoDB to host Terraform state file

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 4.53.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "terraform-state-file-periodic-care-package"
+    key            = "periodic-care-package/import-boostrap/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_state_lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
